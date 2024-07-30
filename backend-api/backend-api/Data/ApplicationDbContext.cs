e5745a1f7e26200c0d143ed16ebf4a3d9d7994e4
sowndv02 <daoson03112002@gmail.com>
2024-07-30 23:07:10 +0700

    [sondv] chore: Update seeding data and update table name of identity

--- backend-api/backend-api/Data/ApplicationDbContext.cs
+++ backend-api/backend-api/Data/ApplicationDbContext.cs
@@ -1,6 +1,10 @@
 ﻿using backend_api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Drawing;
+using System.Reflection.Emit;
 
 namespace backend_api.Data
 {
@@ -10,5 +14,46 @@ namespace backend_api.Data
           : base(options) { }
 
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (!string.IsNullOrEmpty(tableName) && tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+            //SeedData(builder);
+        }
+
+        //Seeding
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
+            {
+                Id = "1",
+                UserName = "admin@admin.com",
+                NormalizedUserName = "admin@admin.com",
+                Email = "admin@admin.com",
+                NormalizedEmail = "admin@admin.com",
+                EmailConfirmed = false,
+                PasswordHash = "admin@admin.com",
+                SecurityStamp = "admin@admin.com",
+                ConcurrencyStamp = "admin@admin.com",
+                PhoneNumber = "0000000000",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0
+            });
+
+           
+        }
+
     }
 }
