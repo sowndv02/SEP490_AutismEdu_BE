20dbf3c3df9cd38000d4bae0c6b012c8dea68868
sowndv02 <daoson03112002@gmail.com>
2024-08-14 00:07:48 +0700

    [sondv] feat: Forgot password func

--- backend-api/backend-api/Controllers/AuthController.cs
+++ backend-api/backend-api/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using backend_api.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
@@ -19,110 +20,239 @@ namespace backend_api.Controllers
         protected APIResponse _response;
         private readonly IMapper _mapper;
         private string audience = string.Empty;
-        public AuthController(IUserRepository userRepository, IMapper mapper, IConfiguration configuration)
+        private readonly IEmailSender _emailSender;
+        public AuthController(IUserRepository userRepository, IMapper mapper, IConfiguration configuration, IEmailSender emailSender)
         {
             _mapper = mapper;
             _userRepository = userRepository;
             _response = new();
+            _emailSender = emailSender;
+        }
+
+
+        [HttpPost("reset-password")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(ResetPasswordDTO model)
+        {
+
+            try
+            {
+                if (model == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Data invalid.");
+                    return BadRequest(_response);
+                }
+                var user = await _userRepository.GetUserByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add($"User not found with email is {model.Email} invalid.");
+                    return BadRequest(_response);
+                }
+                var result = await _userRepository.ResetPasswordAsync(user, model.Code, model.Password);
+                if (!result)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.InternalServerError;
+                    _response.ErrorMessages = new List<string>() { "Internal server error!" };
+                    return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+                }
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
+        }
+
+        [HttpPost("forgot-password")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordDTO forgotPasswordDTO)
+        {
+            
+            try
+            {
+                if (forgotPasswordDTO == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Data invalid.");
+                    return BadRequest(_response);
+                }
+                var user = await _userRepository.GetUserByEmailAsync(forgotPasswordDTO.Email);
+                if(user == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add($"User not found with email is {forgotPasswordDTO.Email} invalid.");
+                    return BadRequest(_response);
+                }
+                var code = await _userRepository.GeneratePasswordResetTokenAsync(user);
+
+                // TODO: UPDATE callback url
+                var callbackUrl = Url.Action("ResetPassword", "Account", new
+                {
+                    userId = user.Id,
+                    code
+                }, protocol: HttpContext.Request.Scheme);
+                await _emailSender.SendEmailAsync(forgotPasswordDTO.Email, "Reset password", $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginRequestDTO model)
         {
-            var tokenDto = await _userRepository.Login(model);
-            if (tokenDto == null)
+            try
             {
-                _response.StatusCode = HttpStatusCode.BadRequest;
-                _response.IsSuccess = false;
-                _response.ErrorMessages.Add("User is currently locked out.");
-                return BadRequest(_response);
+                var tokenDto = await _userRepository.Login(model);
+                if (tokenDto == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("User is currently locked out.");
+                    return BadRequest(_response);
+                }
+                if (string.IsNullOrEmpty(tokenDto.AccessToken))
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Username or password is incorrect");
+                    return BadRequest(_response);
+                }
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Result = tokenDto;
+                return Ok(_response);
             }
-            if (string.IsNullOrEmpty(tokenDto.AccessToken))
+            catch (Exception ex)
             {
-                _response.StatusCode = HttpStatusCode.BadRequest;
                 _response.IsSuccess = false;
-                _response.ErrorMessages.Add("Username or password is incorrect");
-                return BadRequest(_response);
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
             }
-            _response.StatusCode = HttpStatusCode.OK;
-            _response.IsSuccess = true;
-            _response.Result = tokenDto;
-            return Ok(_response);
         }
 
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterationRequestDTO model)
         {
-            bool ifUserNameUnique = _userRepository.IsUniqueUser(model.UserName);
-            if (!ifUserNameUnique)
+            try
             {
-                _response.StatusCode = HttpStatusCode.BadRequest;
-                _response.IsSuccess = false;
-                _response.ErrorMessages.Add("Username already exists");
-                return BadRequest(_response);
-            }
+                bool ifUserNameUnique = _userRepository.IsUniqueUser(model.UserName);
+                if (!ifUserNameUnique)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Username already exists");
+                    return BadRequest(_response);
+                }
 
-            var user = await _userRepository.Register(model);
-            if (user == null)
+                var user = await _userRepository.Register(model);
+                if (user == null)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages.Add("Error while registering");
+                    return BadRequest(_response);
+                }
+                var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+                user.ImageUrl = baseUrl + $"/{SD.UrlImageUser}/" + SD.UrlImageAvatarDefault;
+                user.ImageLocalPathUrl = @"wwwroot\UserImages\" + SD.UrlImageAvatarDefault;
+                user.CreatedDate = DateTime.Now;
+                await _userRepository.UpdateAsync(user);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }catch (Exception ex)
             {
-                _response.StatusCode = HttpStatusCode.BadRequest;
                 _response.IsSuccess = false;
-                _response.ErrorMessages.Add("Error while registering");
-                return BadRequest(_response);
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
             }
-            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
-            user.ImageUrl = baseUrl + $"/{SD.UrlImageUser}/" + SD.UrlImageAvatarDefault;
-            user.ImageLocalPathUrl = @"wwwroot\UserImages\" + SD.UrlImageAvatarDefault;
-            user.CreatedDate = DateTime.Now;
-            await _userRepository.UpdateAsync(user);
-            _response.StatusCode = HttpStatusCode.OK;
-            _response.IsSuccess = true;
-            return Ok(_response);
         }
 
         [HttpPost("refresh")]
         public async Task<IActionResult> GetNewTokenFromRefreshToken([FromBody] TokenDTO model)
         {
-            if (ModelState.IsValid)
+            try
             {
-                var tokenDTOResponse = await _userRepository.RefreshAccessToken(model);
-                if (tokenDTOResponse == null || string.IsNullOrEmpty(tokenDTOResponse.AccessToken))
+                if (ModelState.IsValid)
                 {
-                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    var tokenDTOResponse = await _userRepository.RefreshAccessToken(model);
+                    if (tokenDTOResponse == null || string.IsNullOrEmpty(tokenDTOResponse.AccessToken))
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        _response.ErrorMessages.Add("Token Invalid");
+                        return BadRequest(_response);
+                    }
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    _response.Result = tokenDTOResponse;
+                    return Ok(_response);
+                }
+                else
+                {
                     _response.IsSuccess = false;
-                    _response.ErrorMessages.Add("Token Invalid");
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Error while refresh token");
                     return BadRequest(_response);
                 }
-                _response.StatusCode = HttpStatusCode.OK;
-                _response.IsSuccess = true;
-                _response.Result = tokenDTOResponse;
-                return Ok(_response);
             }
-            else
+            catch (Exception ex)
             {
                 _response.IsSuccess = false;
-                _response.StatusCode = HttpStatusCode.BadRequest;
-                _response.ErrorMessages.Add("Error while refresh token");
-                return BadRequest(_response);
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
             }
         }
 
         [HttpPost("revoke")]
         public async Task<IActionResult> RevokeRefreshToken([FromBody] TokenDTO model)
         {
-            if (ModelState.IsValid)
+            try
             {
-                await _userRepository.RevokeRefreshToken(model);
-                _response.IsSuccess = true;
-                _response.StatusCode = HttpStatusCode.OK;
-                return Ok(_response);
+                if (ModelState.IsValid)
+                {
+                    await _userRepository.RevokeRefreshToken(model);
+                    _response.IsSuccess = true;
+                    _response.StatusCode = HttpStatusCode.OK;
+                    return Ok(_response);
+                }
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Result = "Invalid Input";
+                return BadRequest(_response);
             }
-            _response.IsSuccess = false;
-            _response.StatusCode = HttpStatusCode.BadRequest;
-            _response.Result = "Invalid Input";
-            return BadRequest(_response);
-
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+                return StatusCode((int)HttpStatusCode.InternalServerError, _response);
+            }
         }
 
         
