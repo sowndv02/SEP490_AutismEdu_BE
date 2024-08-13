270723e8056d7b2fc09ebdbbfd84d0a308c51b49
khaidqhe163 <167944897+khaidqhe163@users.noreply.github.com>
2024-08-13 17:50:15 +0700

    [khaidq] <feat>: create front-end login

--- frontend/src/pages/Auth/Login/index.jsx
+++ frontend/src/pages/Auth/Login/index.jsx
@@ -1,150 +1,131 @@
-import { AccountCircle, Visibility, VisibilityOff } from "@mui/icons-material";
-import React, { useState } from "react";
-import LockIcon from "@mui/icons-material/Lock";
-import { Formik, useFormik } from "formik";
-// import { services } from "~/plugins/services";
-import {
-  Box,
-  Button,
-  Container,
-  FormHelperText,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Typography,
-} from "@mui/material";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { Box, Divider, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput, SvgIcon, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import TrelloIcon from '~/assets/trello.svg?react';
+import GoogleIcon from '@mui/icons-material/Google';
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
-
-  const validate = (values) => {
-    const errors = {};
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
-
-    return errors;
-  };
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validate,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [email, setEmail] = useState()
   const INPUT_CSS = {
-    "& label": { color: "black" },
-    "& input": { color: "black" },
-    "& label.Mui-focused": { color: "black" },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": { borderColor: "black" },
-      "&:hover fieldset": { borderColor: "black" },
-      "&.Mui-focused fieldset": { borderColor: "black" },
-    },
+    width: "100%",
+    borderRadius: "15px",
+    ".MuiFormHelperText-root": {
+      color: "red"
+    }
   };
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-
   const handleClickShowPassword = () => setShowPassword((show) => !show);
-  return (
-    <Container
-      maxWidth={false}
-      disableGutters
-      sx={{ bgcolor: "primary.main", height: "100vh", pt: "150px" }}
-    >
-      <Box
-        sx={{
-          width: "400px",
-          margin: "auto",
-          textAlign: "center",
-          border: "1px solid white",
-          padding: "30px",
-          borderRadius: "10px",
-          bgcolor: "white",
-        }}
-      >
-        <Typography align="center" variant="h4" sx={{ fontWeight: "bold" }}>
-          LOGIN
-        </Typography>
-        <form onSubmit={formik.handleSubmit}>
-          <TextField
-            id="email"
-            label="Email"
-            name="email"
-            variant="outlined"
-            fullWidth
-            sx={INPUT_CSS}
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            }}
-          />
-          {formik.errors.email ? (
-            <Typography
-              sx={{
-                color: "error.main",
-                fontSize: "12px",
-                textAlign: "left",
-              }}
-            >
-              {formik.errors.email}
-            </Typography>
-          ) : null}
-          <br></br>
-          <TextField
-            id="password"
-            label="Password"
-            variant="outlined"
-            name="password"
-            type={showPassword ? "text" : "password"}
-            fullWidth
-            sx={{ ...INPUT_CSS, marginTop: "15px" }}
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockIcon />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
 
-          <Button
-            variant="contained"
-            sx={{ mt: "20px" }}
-            fullWidth
-            type="submit"
-          >
-            LOGIN
-          </Button>
-        </form>
-      </Box>
-    </Container>
+  const checkValid = (e, field) => {
+    if (field === 1) {
+      if (e.target.value === "") {
+        setEmailError("Please enter email / username")
+      } else if (e.target.value.length < 6) {
+        setEmailError("Username must be more than 6 characters")
+      } else {
+        setEmailError(null)
+      }
+    }
+    if (field === 2) {
+      if (e.target.value === "") {
+        setPasswordError("Please enter password")
+      } else if (e.target.value.length < 6) {
+        setPasswordError("Password must be more than 6 characters")
+      } else {
+        setPasswordError(null)
+      }
+    }
+  }
+  const handleSubmit = () => {
+
+  }
+  return (
+    <Box sx={{ bgcolor: "#f7f7f9", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <Card sx={{
+        width: "450px",
+        height: "614px",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+        borderRadius: "10px",
+        p: "28px"
+      }}>
+        <CardContent>
+          <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", gap: 1 }}>
+            <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'blue' }} />
+            <Typography sx={{ fontSize: 20, fontWeight: "bold", color: "text.secondary" }}>
+              My App
+            </Typography>
+          </Box>
+          <Typography variant='h5' sx={{ color: "text.secondary", mt: "20px" }}>Welcome to MyApp! 👋</Typography>
+          <Typography sx={{ mt: "10px" }}>Please sign-in to your account and start the adventure</Typography>
+          <Box mt="30px">
+            <FormControl sx={{ ...INPUT_CSS }} variant="outlined">
+              <InputLabel htmlFor="email">Email or Username</InputLabel>
+              <OutlinedInput id="email" label="Email or username" variant="outlined" type='email'
+                onChange={(e) => { checkValid(e, 1) }}
+                error={emailError} />
+              {
+                emailError && (
+                  <FormHelperText error id="accountId-error">
+                    {emailError}
+                  </FormHelperText>
+                )
+              }
+            </FormControl>
+            <FormControl sx={{ ...INPUT_CSS, mt: "20px" }} variant="outlined">
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <OutlinedInput
+                error={passwordError}
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                onChange={(e) => { checkValid(e, 2) }}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+              {
+                passwordError && (
+                  <FormHelperText error id="accountId-error">
+                    {passwordError}
+                  </FormHelperText>
+                )
+              }
+            </FormControl>
+          </Box>
+          <Box sx={{ width: "100%", textAlign: "end", marginTop: "15px" }}>
+            <a href='' style={{ color: "#666cff" }}>Forgot Password?</a>
+          </Box>
+          <Button variant='contained' sx={{ width: "100%", marginTop: "20px" }}>Sign In</Button>
+
+          <Typography sx={{ textAlign: "center", mt: "20px" }}>New on our platform? <a href='' style={{ color: "#666cff" }}>Create an account</a></Typography>
+          <Divider sx={{ mt: "15px" }}>or</Divider>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <IconButton>
+              <GoogleIcon sx={{ color: "#dd4b39 " }} />
+            </IconButton>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
