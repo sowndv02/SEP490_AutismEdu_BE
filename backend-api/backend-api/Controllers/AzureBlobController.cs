13b8fddac2c336ada81c0ea156fc83ba79459d2c
sowndv02 <daoson03112002@gmail.com>
2024-08-15 00:20:50 +0700

    [sondv] feat: Create repo for blob storage and config azure blob storage

---
+++ backend-api/backend-api/Controllers/AzureBlobController.cs
@@ -0,0 +1,27 @@
+﻿using backend_api.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_api.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersionNeutral]
+    public class AzureBlobController : ControllerBase
+    {
+        private IBlobStorageRepository _blobStorageRepository;
+        public AzureBlobController(IBlobStorageRepository blobStorageRepository)
+        {
+            _blobStorageRepository = blobStorageRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImg(IFormFile file)
+        {
+            using var stream = file.OpenReadStream();
+            var fileName = file.FileName;
+            var url = await _blobStorageRepository.UploadImg(stream, fileName); 
+            return Ok(url);
+        }
+    }
+}
