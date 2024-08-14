71660c69e424d50f416bd385d17e2b483fbd4d2d
sowndv02 <daoson03112002@gmail.com>
2024-08-14 22:22:06 +0700

    [sondv] fix: Update profile deploy

--- backend-api/backend-api/Program.cs
+++ backend-api/backend-api/Program.cs
@@ -156,7 +156,7 @@ app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "SEP490");
-    //options.RoutePrefix = string.Empty;
+    options.RoutePrefix = string.Empty;
 });
 
 
