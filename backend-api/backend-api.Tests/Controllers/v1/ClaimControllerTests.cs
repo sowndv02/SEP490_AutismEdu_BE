c06d7be18fefeea126a0a00f45f49f42a8e30866
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-08-19 15:15:59 +0700

    [sondv] test: Unit Test for claim controller

---
+++ backend-api/backend-api.Tests/Controllers/v1/ClaimControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace backend_api.Controllers.v1.Tests
+{
+    public class ClaimControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public ClaimControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+        [Fact()]
+        public async Task GetAllClaimsAsyncTestAsync()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/v1/claim?pageNumber=1");
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
