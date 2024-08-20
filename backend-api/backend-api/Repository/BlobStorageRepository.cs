7b62a926abd0408a438b2e674f0b65546073955b
sowndv02 <daoson03112002@gmail.com>
2024-08-20 20:31:52 +0700

    [sondv] fix: Create gen sas blob try catch

--- backend-api/backend-api/Repository/BlobStorageRepository.cs
+++ backend-api/backend-api/Repository/BlobStorageRepository.cs
@@ -7,30 +7,39 @@ namespace backend_api.Repository
     public class BlobStorageRepository : IBlobStorageRepository
     {
         private string ConnectionString = string.Empty;
-        private string LogosContainerName = string.Empty;
+        private string LogosContainerName_Private = string.Empty;
+        private string LogosContainerName_Public = string.Empty;
         private string AccountKey = string.Empty;
         public BlobStorageRepository(IConfiguration configuration)
         {
             ConnectionString = configuration.GetValue<string>("BlobStorage:ConnectionString");
-            LogosContainerName = configuration.GetValue<string>("BlobStorage:LogosContainerName");
+            LogosContainerName_Private = configuration.GetValue<string>("BlobStorage:LogosContainerName_Private");
+            LogosContainerName_Public = configuration.GetValue<string>("BlobStorage:LogosContainerName_Public");
             AccountKey = configuration.GetValue<string>("BlobStorage:AccountKey");
         }
 
         public string GetBlobSasUrl(string? blobUrl)
         {
-            if (blobUrl == null) return null;
-            var sasBuilder = new BlobSasBuilder()
+            try
             {
-                BlobContainerName = LogosContainerName,
-                Resource = "b",
-                StartsOn = DateTime.Now,
-                ExpiresOn = DateTime.Now.AddMinutes(30),
-                BlobName = GetBlobNameFromUrl(blobUrl)
-            };
-            var blobServiceClient = new BlobServiceClient(ConnectionString);
-            sasBuilder.SetPermissions(BlobSasPermissions.Read);
-            var sasToken = sasBuilder.ToSasQueryParameters(new Azure.Storage.StorageSharedKeyCredential(blobServiceClient.AccountName, AccountKey)).ToString();
-            return $"{blobUrl}?{sasToken}";
+                if (blobUrl == null) return null;
+                var sasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = LogosContainerName_Private,
+                    Resource = "b",
+                    StartsOn = DateTimeOffset.UtcNow,
+                    ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(30),
+                    BlobName = GetBlobNameFromUrl(blobUrl)
+                };
+                var blobServiceClient = new BlobServiceClient(ConnectionString);
+                sasBuilder.SetPermissions(BlobSasPermissions.Read);
+                var sasToken = sasBuilder.ToSasQueryParameters(new Azure.Storage.StorageSharedKeyCredential(blobServiceClient.AccountName, AccountKey)).ToString();
+                return $"{blobUrl}?{sasToken}";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         private string GetBlobNameFromUrl(string blobUrl)
@@ -39,15 +48,25 @@ namespace backend_api.Repository
             return uri.Segments.Last();
         }
 
-        public async Task<string> UploadImg(Stream data, string fileName)
+        public async Task<string> UploadImg(Stream data, string fileName, bool isPrivate = false)
         {
-            var blobServiceClient = new BlobServiceClient(ConnectionString);
-            var containerClient = blobServiceClient.GetBlobContainerClient(LogosContainerName);
+            try
+            {
+                var blobServiceClient = new BlobServiceClient(ConnectionString);
+                var containerClient = blobServiceClient.GetBlobContainerClient(LogosContainerName_Public);
 
-            var blobClient = containerClient.GetBlobClient(fileName);
-            await blobClient.UploadAsync(data);
-            var blobUrl = blobClient.Uri.ToString();
-            return blobUrl;
+                if (isPrivate)
+                    containerClient = blobServiceClient.GetBlobContainerClient(LogosContainerName_Private);
+
+                var blobClient = containerClient.GetBlobClient(fileName);
+                await blobClient.UploadAsync(data);
+                var blobUrl = blobClient.Uri.ToString();
+                return blobUrl;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
