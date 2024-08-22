8951ce7599900a41ef4dd3f3c797319b246739da
sowndv02 <daoson03112002@gmail.com>
2024-08-22 22:35:25 +0700

    [sondv] merge: Merge front-end to main

--- frontend/src/layout/AdminLayout.jsx
+++ frontend/src/layout/AdminLayout.jsx
@@ -1,9 +1,7 @@
 import { Box } from '@mui/material'
-import AdminHeader from '~/Components/AdminHeader'
-import AdminLeftBar from '~/Components/AdminLeftBar'
 import { Outlet } from 'react-router-dom';
 import AdminHeader from '~/components/AdminHeader';
-import AdminLeftBar from '~/components/AdminHeader';
+import AdminLeftBar from '~/components/AdminLeftBar';
 function AdminLayout() {
     return (
         <Box sx={{ bgcolor: "#f7f7f9", height: "100vh", display: "flex", gap: "2" }}>
