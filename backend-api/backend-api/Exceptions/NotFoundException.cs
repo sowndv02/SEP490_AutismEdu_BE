38162d26f354ef1a49a44e31b02488cba72ed938
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-08-28 00:41:56 +0700

    [sondv] feat: Add exception middleware

---
+++ backend-api/backend-api/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_api.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key) : base($"{name} ({key}) was not found")
+        {
+
+        }
+    }
+}
