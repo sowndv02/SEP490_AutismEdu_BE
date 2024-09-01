dafb7ccbf84ec4996fd01d797b72a6e3e47951c3
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-09-01 11:02:40 +0700

    [sondv] fix: Update reqs

--- backend-api/backend-api/SD.cs
+++ backend-api/backend-api/SD.cs
@@ -9,7 +9,7 @@
         public const string UrlImageDefault = "https://placehold.co/600x400";
         public const string UrlImageAvatarDefault = "default-avatar.png";
         public const string URL_FE = "http://localhost:5173";
-        public const string URL_IMAGE_DEFAULT_BLOB = "https://sondvstorage.blob.core.windows.net/logos-public/default-avatar.png";
+        public const string URL_IMAGE_DEFAULT_BLOB = "https://storagesep.blob.core.windows.net/logos-public/default-avatar.png";
         public const string APPLICATION_USER = "ApplicationUser";
         public const string GOOGLE_USER = "GoogleUser";
         public const string FOLDER_NAME_LOG_EMAIL = "MailSave";
