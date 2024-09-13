aec65d77cc8cec57618c65b400ad97a0ee10bdea
vuanh957 <vuanh957@gmail.com>
2024-09-14 01:10:44 +0700

    [anhthv] feat: Update Db 14/9

---
+++ backend-api/backend-api/Models/ReportMedia.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class ReportMedia
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public string UrlMedia { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+        [ForeignKey(nameof(ReportId))]
+        public Report Report { get; set; }
+    }
+}
