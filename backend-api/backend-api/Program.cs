2d6aa8c1ff9a362c9cf9dbbe5dc5ddc628531b6f
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-09-18 12:39:35 +0700

    [sondv] fix: Exception by cache

--- backend-api/backend-api/Program.cs
+++ backend-api/backend-api/Program.cs
@@ -207,17 +207,17 @@ app.UseHttpsRedirection();
 app.UseResponseCaching();
 
 // Add cache for response
-app.Use(async (context, next) =>
-{
-    context.Response.GetTypedHeaders().CacheControl =
-    new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
-    {
-        Public = true,
-        MaxAge = TimeSpan.FromSeconds(10)
-    };
-    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] = new string[] { "Accept-Encoding" };
-    await next();
-});
+//app.Use(async (context, next) =>
+//{
+//    context.Response.GetTypedHeaders().CacheControl =
+//    new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+//    {
+//        Public = true,
+//        MaxAge = TimeSpan.FromSeconds(10)
+//    };
+//    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] = new string[] { "Accept-Encoding" };
+//    await next();
+//});
 
 app.UseAuthentication();
 app.UseAuthorization();
