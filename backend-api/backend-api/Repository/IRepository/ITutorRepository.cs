ca8c990fc7af8b21a91922917a70eb13261e27cb
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-09-23 22:10:49 +0700

    [sondv] feat: Create tutor repository

---
+++ backend-api/backend-api/Repository/IRepository/ITutorRepository.cs
@@ -0,0 +1,9 @@
+﻿using backend_api.Models;
+
+namespace backend_api.Repository.IRepository
+{
+    public interface ITutorRepository : IRepository<Tutor>
+    {
+        Task<Tutor> UpdateAsync(Tutor tutor);
+    }
+}
