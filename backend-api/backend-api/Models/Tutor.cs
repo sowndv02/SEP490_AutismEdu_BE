25f4250d3290dea5930da1700c5dc6bd1cee35a2
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-09-23 10:54:46 +0700

    [sondv] feat: Crete table Tutor

---
+++ backend-api/backend-api/Models/Tutor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class Tutor
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string FormalName { get; set; }
+        public decimal? PriceFrom { get; set; }
+        public decimal? PriceTo { get; set; }
+        public int StartAge { get; set; }
+        public int EndAge { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
