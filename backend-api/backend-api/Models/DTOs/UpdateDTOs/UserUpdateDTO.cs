855d819d48a43b36e67a56e9b1518a4cd73959b9
sowndv02 <daoson03112002@gmail.com>
2024-09-29 01:03:14 +0700

    [sondv] fix: Refactor src and get list request tutor

--- backend-api/backend-api/Models/DTOs/UpdateDTOs/UserUpdateDTO.cs
+++ backend-api/backend-api/Models/DTOs/UpdateDTOs/UserUpdateDTO.cs
@@ -2,9 +2,8 @@
 {
     public class UserUpdateDTO
     {
-        public string UserId { get; set; }
-        public string FullName { get; set; }
         public string? PhoneNumber { get; set; }
+        public string? FullName { get; set; }
         public string? Address { get; set; }
         public string? ImageUrl { get; set; }
         public string? ImageLocalPathUrl { get; set; }
