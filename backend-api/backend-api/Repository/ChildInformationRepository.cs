20cce5d8652e49a42c7f1efc0a7d93ce74bc4e89
vuanh957 <vuanh957@gmail.com>
2024-10-05 15:10:35 +0700

    [anhthv] feat: Create API create and get child info

---
+++ backend-api/backend-api/Repository/ChildInformationRepository.cs
@@ -0,0 +1,53 @@
+﻿using backend_api.Data;
+using backend_api.Models;
+using backend_api.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_api.Repository
+{
+    public class ChildInformationRepository : Repository<ChildInformation>, IChildInformationRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public ChildInformationRepository(UserManager<ApplicationUser> userManager, ApplicationDbContext context) : base(context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<List<ChildInformation>> GetChildByParentEmailAsync(string email)
+        {
+            try
+            {
+                var parent = await _userManager.FindByEmailAsync(email);
+                if (parent != null)
+                {
+                    string parentId = parent.Id;
+                    var childInfoList = await _context.ChildInformations.Where(c => c.ParentId == parentId).ToListAsync();
+                    return childInfoList;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<ChildInformation> UpdateAsync(ChildInformation model)
+        {
+            try
+            {
+                _context.ChildInformations.Update(model);
+                await _context.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
