529ee06195f052c6d82209763426104849728c78
vuanh957 <vuanh957@gmail.com>
2024-10-05 23:26:26 +0700

    [anhthv] feat: Create table AvailableTime and AvailableTimeSlot

---
+++ backend-api/backend-api/Models/AvailableTimeSlot.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class AvailableTimeSlot
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int WeekdayId { get; set; }
+        public TimeSpan From { get; set; }
+        public TimeSpan To { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+        [ForeignKey(nameof(WeekdayId))]
+        public AvailableTime Weekday { get; set; }
+
+    }
+}
