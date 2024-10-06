e50ff7e879b3cc0db9142e92143482c39ece26a5
sowndv02 <daoson03112002@gmail.com>
2024-10-06 17:56:38 +0700

    [sondv] feat: Create api create tutor request

---
+++ backend-api/backend-api/Models/DTOs/CreateDTOs/TutorRequestCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models.DTOs.CreateDTOs
+{
+    public class TutorRequestCreateDTO
+    {
+        public string TutorId { get; set; }
+        public int ChildId { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ backend-api/backend-api/Models/DTOs/TutorRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using static backend_api.SD;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models.DTOs
+{
+    public class TutorRequestDTO
+    {
+        public int Id { get; set; }
+        public ChildInformationDTO ChildInformation { get; set; }
+        public ApplicationUserDTO Parent { get; set; }
+        public string? Description { get; set; }
+        public Status RequestStatus { get; set; }
+        public string? RejectionReason { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
