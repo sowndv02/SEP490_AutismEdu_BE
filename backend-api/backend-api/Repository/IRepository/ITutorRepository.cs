00202487b58718bef9baf21ead796ef80dde3ec6
sowndv02 <daoson03112002@gmail.com>
2024-10-06 17:57:08 +0700

    [sondv] fix: Update search tutor

--- backend-api/backend-api/Repository/IRepository/ITutorRepository.cs
+++ backend-api/backend-api/Repository/IRepository/ITutorRepository.cs
@@ -1,9 +1,13 @@
 ﻿using backend_api.Models;
+using System.Linq.Expressions;
 
 namespace backend_api.Repository.IRepository
 {
     public interface ITutorRepository : IRepository<Tutor>
     {
         Task<Tutor> UpdateAsync(Tutor tutor);
+        Task<(int TotalCount, List<Tutor> list)> GetAllTutorAsync(Expression<Func<Tutor, bool>>? filter = null, Expression<Func<Tutor, bool>>? filterAddress = null, int? reviewScore = 5, 
+            int? ageFrom = 0, int? ageTo = 15, string? includeProperties = null,
+            int pageSize = 10, int pageNumber = 1);
     }
 }
