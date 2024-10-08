b541d510340320427789c9ed14060702dd877753
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-10-08 10:48:17 +0700

    [sondv] fix: Update response status certificate

--- backend-api/backend-api/Models/DTOs/CertificateDTO.cs
+++ backend-api/backend-api/Models/DTOs/CertificateDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace backend_api.Models.DTOs
+﻿using static backend_api.SD;
+
+namespace backend_api.Models.DTOs
 {
     public class CertificateDTO
     {
         public int Id { get; set; }
         public string CertificateName { get; set; }
+        public ApplicationUserDTO? ApprovedBy { get; set; }
         public string? IdentityCardNumber { get; set; }
         public string? IssuingInstitution { get; set; }
         public DateTime? IssuingDate { get; set; }
@@ -11,7 +14,7 @@
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime? UpdatedDate { get; set; }
         public string? Feedback { get; set; }
-        public bool? IsApprove { get; set; }
+        public Status RequestStatus { get; set; }
         public List<CertificateMediaDTO> CertificateMedias { get; set; }
     }
 }
