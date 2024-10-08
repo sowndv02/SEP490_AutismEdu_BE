aba214a1e2675216c1a4b1f3c9bbbee320e9d81b
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-10-08 11:44:08 +0700

    [sondv] feat: Add repository blog

---
+++ backend-api/backend-api/Repository/BlogRepository.cs
@@ -0,0 +1,29 @@
+﻿using backend_api.Data;
+using backend_api.Models;
+using backend_api.Repository.IRepository;
+
+namespace backend_api.Repository
+{
+    public class BlogRepository : Repository<Blog>, IBlogRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BlogRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Blog> UpdateAsync(Blog model)
+        {
+            try
+            {
+                _context.Blogs.Update(model);
+                await _context.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
