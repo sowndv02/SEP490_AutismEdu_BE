6afffab61088c5c6bcc8dab045d2df2e1b28527a
vuanh957 <vuanh957@gmail.com>
2024-10-09 19:03:08 +0700

    [anhthv] fix: update removeTimeSlot and table childInfo

--- backend-api/backend-api/Migrations/20241009113752_UpdateAssessmentQuestion.Designer.cs
+++ backend-api/backend-api/Migrations/20241009113752_UpdateAssessmentQuestion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend_api.Data;
 
@@ -11,9 +12,10 @@ using backend_api.Data;
 namespace backend_api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241009113752_UpdateAssessmentQuestion")]
+    partial class UpdateAssessmentQuestion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -398,6 +400,9 @@ namespace backend_api.Migrations
 
                     b.Property<DateTime>("CreatedDate")
                         .HasColumnType("datetime2");
+
+                    b.Property<string>("InitialCondition")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
                         .HasColumnType("nvarchar(max)");
---
+++ backend-api/backend-api/Migrations/20241009115950_UpdateChildInfoTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_api.Migrations
+{
+    public partial class UpdateChildInfoTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitialCondition",
+                table: "ChildInformations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InitialCondition",
+                table: "ChildInformations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
