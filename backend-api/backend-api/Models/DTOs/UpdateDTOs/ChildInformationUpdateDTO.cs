58bed9bc37d022617544647a6e83371ba0c7046f
vuanh957 <vuanh957@gmail.com>
2024-10-13 23:31:55 +0700

    [anhthv] feat: Create api updateChildInfo

---
+++ backend-api/backend-api/Models/DTOs/UpdateDTOs/ChildInformationUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_api.Models.DTOs.UpdateDTOs
+{
+    public class ChildInformationUpdateDTO
+    {
+        [Required]
+        public int ChildId { get; set; }
+        public string? Name { get; set; }
+        public bool isMale { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
