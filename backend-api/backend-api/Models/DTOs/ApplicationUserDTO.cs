609aa69fbbaf9f164795c4d5cc146ea83456eb99
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-10-16 11:36:27 +0700

    [sondv] fix: Update res address

--- backend-api/backend-api/Models/DTOs/ApplicationUserDTO.cs
+++ backend-api/backend-api/Models/DTOs/ApplicationUserDTO.cs
@@ -5,6 +5,7 @@
         public string Id { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
+        public string Address { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Role { get; set; }
         public string? UserClaim { get; set; }
