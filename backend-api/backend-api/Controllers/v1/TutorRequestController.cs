c2c09109bb9d78e8979cf09784f00ae52b61c155
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-10-16 11:25:45 +0700

    [sondv] fix: Update user, update request tutor

--- backend-api/backend-api/Controllers/v1/TutorRequestController.cs
+++ backend-api/backend-api/Controllers/v1/TutorRequestController.cs
@@ -3,11 +3,8 @@ using backend_api.Models;
 using backend_api.Models.DTOs;
 using backend_api.Models.DTOs.CreateDTOs;
 using backend_api.Models.DTOs.UpdateDTOs;
-using backend_api.Repository;
 using backend_api.Repository.IRepository;
 using backend_api.Utils;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Expressions;
 using System.Net;
@@ -121,7 +118,7 @@ namespace backend_api.Controllers.v1
                     return BadRequest(_response);
                 }
                 TutorRequest model = _mapper.Map<TutorRequest>(tutorRequestCreateDTO);
-                model.TutorId = userId;
+                model.ParentId = userId;
                 model.CreatedDate = DateTime.Now;
                 await _tutorRequestRepository.CreateAsync(model);
                 _response.StatusCode = HttpStatusCode.Created;
