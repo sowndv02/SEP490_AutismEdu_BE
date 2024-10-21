bcde16fda63f6f4a1afdbcf54f54b35f5dc3bfe5
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-10-22 00:10:54 +0700

    [sondv] fix: Generate schedule with correct time

--- backend-api/backend-api/Services/GenerateScheduleTimeSlot.cs
+++ backend-api/backend-api/Services/GenerateScheduleTimeSlot.cs
@@ -19,11 +19,26 @@ namespace backend_api.Services
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken); // Run daily
+                DateTime now = DateTime.Now;
+                DateTime nextRunTime = GetNextSundayAt5AM(now);
+
+                TimeSpan delay = nextRunTime - now;
+
+                if (delay.TotalMilliseconds > 0)
+                {
+                    await Task.Delay(delay, stoppingToken);
+                }
+                //await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
                 await GenerateScheduleTimeSlotForTutor();
+                await Task.Delay(TimeSpan.FromDays(7), stoppingToken);
             }
         }
-
+        private DateTime GetNextSundayAt5AM(DateTime currentTime)
+        {
+            int daysUntilSunday = ((int)DayOfWeek.Sunday - (int)currentTime.DayOfWeek + 7) % 7;
+            DateTime nextSunday = currentTime.Date.AddDays(daysUntilSunday);
+            return nextSunday.AddHours(5);
+        }
         private async Task GenerateScheduleTimeSlotForTutor()
         {
             using (var scope = _serviceProvider.CreateScope())
@@ -32,14 +47,43 @@ namespace backend_api.Services
                 var scheduleTimeSlots = context.ScheduleTimeSlots.Include(x => x.StudentProfile).Where(x => x.StudentProfile != null && x.StudentProfile.Status == SD.StudentProfileStatus.Teaching).ToList();
                 foreach (var item in scheduleTimeSlots)
                 {
+                    var nextDate = DateTime.Now;
+                    switch (item.Weekday)
+                    {
+                        // CN
+                        case (int)DayOfWeek.Sunday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Sunday);
+                            break;
+                        // T2
+                        case (int)DayOfWeek.Monday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Monday);
+                            break;
+                        case (int)DayOfWeek.Tuesday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Tuesday);
+                            break;
+                        case (int)DayOfWeek.Wednesday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Wednesday);
+                            break;
+                        case (int)DayOfWeek.Thursday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Thursday);
+                            break;
+                        case (int)DayOfWeek.Friday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Friday);
+                            break;
+                        case (int)DayOfWeek.Saturday:
+                            nextDate = GetNextDayOfWeek(DayOfWeek.Saturday);
+                            break;
+                    }
                     var schedule = new Schedule()
                     {
                         TutorId = item.StudentProfile?.TutorId,
-                        ChildId = (int)(item.StudentProfile?.ChildId),
                         AttendanceStatus = SD.AttendanceStatus.NOT_YET,
+                        ScheduleDate = nextDate,
                         CreatedDate = DateTime.Now,
                         PassingStatus = SD.PassingStatus.NOT_YET,
                         UpdatedDate = null,
+                        Start = item.From,
+                        End = item.To,
                         ScheduleTimeSlotId = item.Id
                     };
                     await context.Schedules.AddAsync(schedule);
@@ -49,5 +93,17 @@ namespace backend_api.Services
                 _logger.LogWarning($"Background service generate schedule for tutor. Total {total} records");
             }
         }
+
+        public DateTime GetNextDayOfWeek(DayOfWeek targetDay)
+        {
+            DateTime today = DateTime.Now;
+            int daysUntilNextTargetDay = ((int)targetDay - (int)today.DayOfWeek + 7) % 7;
+            if (daysUntilNextTargetDay == 0)
+            {
+                daysUntilNextTargetDay = 7;
+            }
+            return today.AddDays(daysUntilNextTargetDay);
+        }
+
     }
 }
