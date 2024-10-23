818c475de6e4da06fc388ebc1dab99b4a5e35585
vuanh957 <vuanh957@gmail.com>
2024-10-24 01:17:05 +0700

    [anhthv] fix: remove tutorRequestId from createStudentProfile api

--- backend-api/backend-api/Models/DTOs/CreateDTOs/StudentProfileCreateDTO.cs
+++ backend-api/backend-api/Models/DTOs/CreateDTOs/StudentProfileCreateDTO.cs
@@ -22,8 +22,6 @@ namespace backend_api.Models.DTOs.CreateDTOs
         public int ChildId { get; set; }
         [Required]
         public string InitialCondition { get; set; }
-        [Required]
-        public int TutorRequestId { get; set; }
         public List<InitialAssessmentResultCreateDTO> InitialAssessmentResults { get; set; }
         public List<ScheduleTimeSlotCreateDTO> ScheduleTimeSlots { get; set; }
     }
