d00b7bef7b021ef4725b35742c47657cf68fda28
sowndv02 <daoson03112002@gmail.com>
2024-10-23 20:18:06 +0700

    [sondv] feat: Add config file rabbitMQ and service bus

---
+++ backend-api/backend-api/Models/EmailLogger.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class EmailLogger
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
