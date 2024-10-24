3e8eb657424cf1f19ae451894bfa0f2020cc1b64
vuanh957 <vuanh957@gmail.com>
2024-10-24 22:19:09 +0700

    [anhthv] fix: fix StudentProfileDetailDTO

--- backend-api/backend-api/Models/DTOs/StudentProfileDetailDTO.cs
+++ backend-api/backend-api/Models/DTOs/StudentProfileDetailDTO.cs
@@ -10,11 +10,7 @@
         public DateTime? BirthDate { get; set; }
         public string? InitialCondition { get; set; }
         public SD.StudentProfileStatus Status { get; set; }
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string? PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public string? ImageUrl { get; set; }      
+        public TutorDTO Tutor { get; set; }
         public List<InitialAssessmentResultDTO> InitialAssessmentResults { get; set; }
         public List<ScheduleTimeSlotDTO> ScheduleTimeSlots { get; set; }
     }
