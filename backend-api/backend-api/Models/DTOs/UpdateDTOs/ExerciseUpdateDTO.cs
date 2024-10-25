486ecb29a5a6f2fcaa6d951f12b78043f59216b9
Nguyễn Tuấn Cường <cuongnt0823@gmail.com>
2024-10-25 12:03:17 +0700

    [cuongnt] fix: Update pageSize, sort by CreatedDate, Create Update Exercise

---
+++ backend-api/backend-api/Models/DTOs/UpdateDTOs/ExerciseUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace backend_api.Models.DTOs.UpdateDTOs
+{
+    public class ExerciseUpdateDTO
+    {
+        public string ExerciseName { get; set; }
+        public string ExerciseContent { get; set; }
+    }
+
+}
