c8921204a84c6b12cdfaec55e2a43fc11217e510
vuanh957 <vuanh957@gmail.com>
2024-10-30 00:37:51 +0700

    [anhthv] feat: return created progressReport, update getAll

---
+++ backend-api/backend-api/Models/DTOs/ProgressReportGraphDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_api.Models.DTOs
+{
+    public class ProgressReportGraphDTO
+    {
+        public InitialAssessmentResultDTO InitialAssessmentResultDTO { get; set; }
+        public List<ProgressReportDTO> ProgressReports { get; set; }
+    }
+}
