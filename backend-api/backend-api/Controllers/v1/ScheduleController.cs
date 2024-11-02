de863c9fa3f4a1df84fc7dda2a8c3c27eb37c7ed
sowndv02 <daoson03112002@gmail.com>
2024-11-02 22:14:53 +0700

    [sondv] fix: Update attend schedule

--- backend-api/backend-api/Controllers/v1/ScheduleController.cs
+++ backend-api/backend-api/Controllers/v1/ScheduleController.cs
@@ -170,10 +170,13 @@ namespace backend_api.Controllers.v1
                 model.PassingStatus = updateDTO.PassingStatus;
                 model.UpdatedDate = DateTime.Now;
                 var result = await _scheduleRepository.UpdateAsync(model);
-                var nextSchedule = await _scheduleRepository.GetAsync(x => x.PassingStatus == SD.PassingStatus.NOT_YET && x.AttendanceStatus == SD.AttendanceStatus.NOT_YET && x.ScheduleDate.Date > result.ScheduleDate.Date && x.ExerciseId == result.ExerciseId && x.ExerciseTypeId == x.ExerciseTypeId, false, null);
+                var nextSchedule = await _scheduleRepository.GetAsync(x => x.PassingStatus == SD.PassingStatus.NOT_YET && x.AttendanceStatus == SD.AttendanceStatus.NOT_YET && x.ScheduleDate.Date > result.ScheduleDate.Date && x.ExerciseId == result.ExerciseId && x.ExerciseTypeId == result.ExerciseTypeId, false, null);
+                if (nextSchedule != null)
+                {
+                    _response.ErrorMessages = new List<string>() { _resourceService.GetString(SD.DUPPLICATED_ASSIGN_EXERCISE) };
+                }
                 _response.StatusCode = HttpStatusCode.NoContent;
                 _response.IsSuccess = true;
-                _response.ErrorMessages = new List<string>() { _resourceService.GetString(SD.DUPPLICATED_ASSIGN_EXERCISE) };
                 _response.Result = _mapper.Map<ScheduleDTO>(result);
                 return Ok(_response);
             }
