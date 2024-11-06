3f24e437ead37f0487fe13c90b8b835e9df645c8
vuanh957 <vuanh957@gmail.com>
2024-11-06 17:27:09 +0700

    [anhthv] feat: update studentProfile API

--- backend-api/backend-api/Repository/ScheduleTimeSlotRepository.cs
+++ backend-api/backend-api/Repository/ScheduleTimeSlotRepository.cs
@@ -12,5 +12,19 @@ namespace backend_api.Repository
         {
             _context = context;
         }
+
+        public async Task<ScheduleTimeSlot> UpdateAsync(ScheduleTimeSlot model)
+        {
+            try
+            {
+                _context.ScheduleTimeSlots.Update(model);
+                await _context.SaveChangesAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
