7804d72c21e16f884572dfe87c9b519a778b259c
vuanh957 <vuanh957@gmail.com>
2024-11-07 15:49:32 +0700

    [anhthv] feat: test model

---
+++ backend-api/backend-api/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class Test
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string TestName { get; set; }
+        public string TestDescription { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate {  get; set; }
+    }
+}
