a007e8d696f8e1d45e5e8bedd5a596c38db79a33
vuanh957 <vuanh957@gmail.com>
2024-11-07 15:49:32 +0700

    [anhthv] feat: test model

---
+++ backend-api/backend-api/Models/TestResult.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_api.Models
+{
+    public class TestResult
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int TestId { get; set; }
+        public int QuestionId { get; set; }
+        public int OptionId { get; set; }
+        public int ParentId { get; set; }
+        public int VersionNumber { get; set; } = 1;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+        [ForeignKey(nameof(TestId))]
+        public Test Test { get; set; }
+        [ForeignKey(nameof(QuestionId))]
+        public AssessmentQuestion Question { get; set; }
+        [ForeignKey(nameof(OptionId))]
+        public AssessmentOption Option { get; set; }
+        [ForeignKey(nameof(ParentId))]
+        public ApplicationUser Parent { get; set; }
+    }
+}
