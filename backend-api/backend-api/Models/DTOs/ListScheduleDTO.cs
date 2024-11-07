26d8b95eb934419de831e3d77e817a2a746b237a
vuanh957 <vuanh957@gmail.com>
2024-11-07 19:42:11 +0700

    [anhthv] feat: return maxDate and timeslotId for getAllSchedule

---
+++ backend-api/backend-api/Models/DTOs/ListScheduleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_api.Models.DTOs
+{
+    public class ListScheduleDTO
+    {
+        public DateTime MaxDate { get; set; }
+        public List<ScheduleDTO> Schedules { get; set; }
+    }
+}
