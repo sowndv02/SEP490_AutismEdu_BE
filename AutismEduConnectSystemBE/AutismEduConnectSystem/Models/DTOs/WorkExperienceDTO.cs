fce8bc9d2f5ebdfb69f10be5b525492458767baa
sowndv02 <daoson03112002@gmail.com>
2024-11-14 00:11:55 +0700

    [sondv] fix: Update DTO tutor user info

--- AutismEduConnectSystemBE/AutismEduConnectSystem/Models/DTOs/WorkExperienceDTO.cs
+++ AutismEduConnectSystemBE/AutismEduConnectSystem/Models/DTOs/WorkExperienceDTO.cs
@@ -12,7 +12,7 @@ namespace AutismEduConnectSystem.Models.DTOs
         public DateTime EndDate { get; set; }
         public Status RequestStatus { get; set; }
         public string? RejectionReason { get; set; }
-        public TutorInfoDTO? Submitter { get; set; }
+        public TutorUserInfo? Submitter { get; set; }
         public ApplicationUserDTO? ApprovedBy { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime UpdatedDate { get; set; }
