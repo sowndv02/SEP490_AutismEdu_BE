26c6f89681dff5469e9bc3f3bae812da509ddad7
sowndv02 <daoson03112002@gmail.com>
2024-11-17 23:30:18 +0700

    [sondv] fix: Update conversation

--- AutismEduConnectSystemBE/AutismEduConnectSystem/Models/DTOs/ConversationDetailDTO.cs
+++ AutismEduConnectSystemBE/AutismEduConnectSystem/Models/DTOs/ConversationDetailDTO.cs
@@ -3,8 +3,7 @@
     public class ConversationDetailDTO
     {
         public int Id { get; set; }
-        public TutorDTO Tutor { get; set; }
-        public ApplicationUserDTO Parent { get; set; }
+        public ApplicationUserDTO User { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
