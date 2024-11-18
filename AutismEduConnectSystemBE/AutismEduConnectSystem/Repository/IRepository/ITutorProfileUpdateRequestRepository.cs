29b7383c24d67ca6b46032a4747978a3bd142d2a
sowndv02 <106914515+sowndv02@users.noreply.github.com>
2024-11-18 12:18:55 +0700

    [sondv] fix: Update search by email

--- AutismEduConnectSystemBE/AutismEduConnectSystem/Repository/IRepository/ITutorProfileUpdateRequestRepository.cs
+++ AutismEduConnectSystemBE/AutismEduConnectSystem/Repository/IRepository/ITutorProfileUpdateRequestRepository.cs
@@ -1,9 +1,12 @@
 ﻿using AutismEduConnectSystem.Models;
+using System.Linq.Expressions;
 
 namespace AutismEduConnectSystem.Repository.IRepository
 {
     public interface ITutorProfileUpdateRequestRepository : IRepository<TutorProfileUpdateRequest>
     {
         Task<TutorProfileUpdateRequest> UpdateAsync(TutorProfileUpdateRequest model);
+        Task<(int TotalCount, List<TutorProfileUpdateRequest> list)> GetAllTutorUpdateRequestAsync(Expression<Func<TutorProfileUpdateRequest, bool>>? filterName = null, Expression<Func<TutorProfileUpdateRequest, bool>>? filterOther = null, string? includeProperties = null,
+          int pageSize = 0, int pageNumber = 1, Expression<Func<TutorProfileUpdateRequest, object>>? orderBy = null, bool isDesc = true);
     }
 }
--- AutismEduConnectSystemBE/AutismEduConnectSystem/Repository/TutorProfileUpdateRequestRepository.cs
+++ AutismEduConnectSystemBE/AutismEduConnectSystem/Repository/TutorProfileUpdateRequestRepository.cs
@@ -1,6 +1,8 @@
 ﻿using AutismEduConnectSystem.Data;
 using AutismEduConnectSystem.Models;
 using AutismEduConnectSystem.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace AutismEduConnectSystem.Repository
 {
@@ -24,6 +26,31 @@ namespace AutismEduConnectSystem.Repository
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        public async Task<(int TotalCount, List<TutorProfileUpdateRequest> list)> GetAllTutorUpdateRequestAsync(Expression<Func<TutorProfileUpdateRequest, bool>>? filterName = null,
+           Expression<Func<TutorProfileUpdateRequest, bool>>? filterOther = null,
+           string? includeProperties = null, int pageSize = 0, int pageNumber = 1, Expression<Func<TutorProfileUpdateRequest, object>>? orderBy = null, bool isDesc = true)
+        {
+            IQueryable<TutorProfileUpdateRequest> query = dbset.AsNoTracking();
+            query = query.Include(u => u.Tutor).ThenInclude(x => x.User);
+            if (filterName != null)
+            {
+                query = query.Where(filterName);
+            }
+            if (orderBy != null)
+            {
+                if (isDesc)
+                    query = query.OrderByDescending(orderBy);
+                else
+                    query = query.OrderBy(orderBy);
+            }
+            int totalCount = await query.CountAsync();
+            if (pageSize > 0)
+            {
+                query = query.Skip(pageSize * (pageNumber - 1)).Take(pageSize);
+            }
+            return (totalCount, await query.ToListAsync());
         }
     }
 }
